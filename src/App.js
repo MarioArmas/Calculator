97b302716a5a6e3cc4dbfaaa611643b962eba350
practicasfront <mario.r.armas@gmail.com>
2022-01-16 17:05:52 -0600

    add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import './styles.css'
 
 function App() {
   return (
-    null
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previus-operand">123</div>
+        <div className="current-operand">2434</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
   );
 }
 
